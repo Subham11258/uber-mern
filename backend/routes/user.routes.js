caf1b26679e21c50609de1df712f751fc3b9eced
Subham11258 <subhamguptaslg.1996@gmail.com>
2025-01-15 12:37:28 +0530

    created authentication for user with jwt

---
+++ backend/routes/user.routes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const userController = require('../controllers/user.controller');
+const {body} = require('express-validator');
+const router = express.Router();
+
+router.post('/register',[
+    body('email').isEmail().withMessage("Invalid Email"),
+    body('fullname.firstname').isLength({min:3}).withMessage
+    ("First must be atleast 3 characters long"),
+    body('password').isLength({min:6}).withMessage('Password must be 6 characters long'),
+],userController.registerUser)
+
+
+module.exports = router; 
