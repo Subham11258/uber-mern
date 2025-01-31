90cc21d3fd0c9f164109444fad0e660259d65bbd
Subham11258 <subhamguptaslg.1996@gmail.com>
2025-01-31 15:53:02 +0530

    updating homepage ui

--- frontend/frontend/src/pages/Home.jsx
+++ frontend/frontend/src/pages/Home.jsx
@@ -1,27 +1,39 @@
 import React, { useState, useRef } from 'react';
-import {useGSAP} from '@gsap/react';
+import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
+import 'remixicon/fonts/remixicon.css';
+
 export default function Home() {
   const [pickup, setPickup] = useState('');
   const [destination, setDestination] = useState('');
   const [panelOpen, setPanelOpen] = useState(false);
   const panelRef = useRef(null);
+  const panelCloseRef = useRef(null);
+
   const submitHandler = (e) => {
     e.preventDefault();
     console.log('form submitted');
   };
 
-  useGSAP(()=>{
-    if(panelOpen){
-    gsap.to(panelRef.current,{
-      height: '70%'
-    })
-  }else{
-    gsap.to(panelRef.current,{
-      height: '0%'
-    })
-  }
-  },[panelOpen])
+  useGSAP(() => {
+    if (panelOpen) {
+      gsap.to(panelRef.current, {
+        height: '70%'
+      })
+      gsap.to(panelCloseRef.current, {
+        rotate: 180,
+        opacity: 1
+      })
+    } else {
+      gsap.to(panelRef.current, {
+        height: '0%'
+      })
+      gsap.to(panelCloseRef.current, {
+        rotate: 0,
+        opacity: 0
+      })
+    }
+  }, [panelOpen])
   return (
     <div className="h-screen relative">
       {/* Background Image */}
@@ -41,28 +53,30 @@ export default function Home() {
       {/* Overlay Content */}
       <div className="flex flex-col justify-end h-screen top-0 absolute w-full z-10">
         <div className="h-[30%] p-6 bg-white relative">
+          <h5 ref={panelCloseRef} onClick={()=>setPanelOpen(false)} className="absolute opacity-0 top-6 right-6 text-xl"><i className="ri-arrow-down-wide-line"></i></h5>
           <h4 className="text-2xl font-semibold">Find a trip</h4>
+
           <form onSubmit={(e) => submitHandler(e)}>
             <div className="line absolute h-16 w-1 top-[45%] left-8 bg-gray-900 rounded-full"></div>
             <input
-            onClick={()=>setPanelOpen(true)}
+              onClick={() => setPanelOpen(true)}
               value={pickup}
-              onChange={(e)=>setPickup(e.target.value)}
+              onChange={(e) => setPickup(e.target.value)}
               className="bg-[#eee] px-12 py-2 text-base rounded-lg w-full mt-5"
               type="text"
               placeholder="Enter your location"
             />
             <input
-              onClick={()=>setPanelOpen(true)}
+              onClick={() => setPanelOpen(true)}
               value={destination}
-              onChange={(e)=>setDestination(e.target.value)}
+              onChange={(e) => setDestination(e.target.value)}
               className="bg-[#eee] px-12 py-2 text-base rounded-lg w-full mt-3"
               type="text"
               placeholder="Enter your destination"
             />
           </form>
         </div>
-        <div ref={panelRef} className="bg-red-500 h-0"> 
+        <div ref={panelRef} className="bg-red-500 h-0">
 
         </div>
       </div>
